8a1cb2f0f086c636ac7d62056c1f71fc1798342c
jpoechill <poamrong@rith.com>
2024-01-30 11:31:36 -0800

    add redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import Image from "next/image";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+  router.push('https://typebypo.etsy.com')
+
   return (
     <main className="w-full flex h-full border justify-center items-center">
       <img src="/cho_tbp_012524.svg" alt="" width="400" height="400" />
