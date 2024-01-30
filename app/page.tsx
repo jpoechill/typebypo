94fdcc46bd9fd594695da9bd6839bc574237ba04
jpoechill <poamrong@rith.com>
2024-01-30 11:50:02 -0800

    add redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 
-import Image from "next/image";
+import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter()
-  router.push('https://typebypo.etsy.com')
+  useEffect(() => {
+
+    router.push('https://typebypo.etsy.com')
+  });
+
+
 
   return (
     <main className="w-full flex h-full border justify-center items-center">
